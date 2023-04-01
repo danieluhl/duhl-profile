b813559d6b66af8dca0449fa2f2163bb545c4a7c
Dan Uhl <duhl@wayfair.com>
2023-04-01 11:19:27 -0400

    adds links to projects

--- api/index.go
+++ api/index.go
@@ -63,34 +63,15 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 					<li><a href="https://www.linkedin.com/in/danieluhl/">LinkedIn</a></li>
 				</ul>
 			</article>
+			<article>
+				<p>Here are a couple random side prjects</p>
+				<ul>
+					<li><a href="https://food.reyan.me/">Grocery Shopping Planner</a></li>
+					<li><a href="https://holidays.reyan.me/">Company Holidays Calendar Generator</a></li>
+				</ul>
+			</article>
 		</main>
 	</body>
 </html>
 	`)
 }
-
-// package handler
-
-// import (
-// 	"html/template"
-// 	"log"
-// 	"net/http"
-// )
-
-// func viewHandler(w http.ResponseWriter, r *http.Request) {
-// 	renderTemplate(w)
-// }
-
-// var templates = template.Must(template.ParseFiles("profile.html"))
-
-// func renderTemplate(w http.ResponseWriter) {
-// 	err := templates.ExecuteTemplate(w, "profile.html", "Hello World!")
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 	}
-// }
-
-// func Handler() {
-// 	http.HandleFunc("/", viewHandler)
-// 	log.Fatal(http.ListenAndServe(":8080", nil))
-// }
